dc51076bff6b2fe0c469b8b8e47c7ee69dc47911
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-23 18:28:30 +0300

    Initial commit

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,25 @@
+import React, {useState} from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+
+import  CustomButton  from './CustomButton'
+import { logo, menu, search, thirdweb } from '../assets'
+import { navlinks } from '../constants'
+const NavBar = () => {
+    const navigate = useNavigate();
+    const [isActive, setIsActive] = useState('dashboard');
+    const [toggleDrawer, setToggleDrawer] = useState(false);
+
+    return (
+    <div className='flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6'>
+        <div className='lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52] bg-[#1c1c24]'>
+            <input type='text' />
+            <div className='w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer'>
+                <img src={search} alt='search' className='w-[15px] h-[15px] object-contain' />
+            </div>
+        </div>
+    </div>
+    )
+}
+
+export default NavBar
